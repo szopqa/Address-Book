460ec5fb4abf3a1fe7ce5574032a6e1f01f96238
szopqa <szopqa96@gmail.com>
2017-03-04 17:23:41 +0100

    Adding new contact to contacts list. Chanhes in properties

--- AddressBook/Form1.cs
+++ AddressBook/Form1.cs
@@ -11,20 +11,30 @@ using System.Windows.Forms;
 namespace AddressBook {
 	public partial class Form1 : Form {
 
+		//PROPERTIES
 		private List<People> Contacts = new List<People>();
+		private AddNew_form addWindow = new AddNew_form();
+
+
+
 
 		public Form1 () {
 			InitializeComponent();
 
 			//TODO: Load contacts from file to Contacts List
+			//TODO: Sort them by Surname's first letter
 
 		}
 
 
 		private void AddNewBtn_Click ( object sender, EventArgs e ) {
-			AddNew_form addWindow = new AddNew_form();
 			this.Hide();
 			addWindow.ShowDialog();
+		}
+
+
+		private void AddNewContactToList(People new_person ) {
+			Contacts.Add(addWindow.Person);
 		}
 
 		private void ShowMoreBtn_Click ( object sender, EventArgs e ) {
@@ -42,5 +52,7 @@ namespace AddressBook {
 		private void SettingsBtn_Click( object sender, EventArgs e) {
 
 		}
+
+
 	}
 }
